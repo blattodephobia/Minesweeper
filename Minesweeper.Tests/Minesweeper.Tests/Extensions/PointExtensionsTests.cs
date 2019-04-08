b4a7ab5a1e67f3b065a18bb30978ada600e694ba
blattodephobia <hselldf398@gmail.com>
2019-04-08 19:59:18 +0300

    Added unit test harness, made sure it works with a few easy unit tests.

---
+++ Minesweeper.Tests/Minesweeper.Tests/Extensions/PointExtensionsTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using Minesweeper.Extensions;
+using NUnit.Framework;
+
+namespace Minesweeper.Tests
+{
+    public class PointExtensionsTests
+    {
+        [TestFixture]
+        public class IsInRangeOfTests
+        {
+            [Test]
+            public void ReturnsFalseIfOutOfXRange()
+            {
+                int[,] array = new int[1, 2];
+
+                Point point = new Point(array.GetLength(0), 0);
+
+                Assert.IsFalse(point.IsInRangeOf(array));
+            }
+
+            [Test]
+            public void ReturnsFalseIfOutOfYRange()
+            {
+                int[,] array = new int[1, 2];
+
+                Point point = new Point(0, array.GetLength(1));
+
+                Assert.IsFalse(point.IsInRangeOf(array));
+            }
+
+            [Test]
+            public void ReturnsFalseIfOutOfXYRange()
+            {
+                int[,] array = new int[1, 2];
+
+                Point point = new Point(array.GetLength(0), array.GetLength(1));
+
+                Assert.IsFalse(point.IsInRangeOf(array));
+            }
+
+            [Test]
+            public void ReturnsTrueIfInXYRange()
+            {
+                int[,] array = new int[1, 2];
+
+                Point point = new Point(0, 0);
+
+                Assert.IsTrue(point.IsInRangeOf(array));
+            }
+
+            [Test]
+            public void ReturnsFalseOnEmptyArray()
+            {
+                int[,] array = new int[0, 0];
+
+                Point point = new Point(0, 0);
+
+                Assert.IsFalse(point.IsInRangeOf(array));
+            }
+        }
+    }
+}
---
+++ Minesweeper.Tests/Minesweeper.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Minesweeper.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Minesweeper.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6703b927-bf47-44b1-9254-0b9c0f0a92d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
