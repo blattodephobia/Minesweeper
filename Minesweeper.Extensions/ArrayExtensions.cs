a227144fbd2efd6d1e4c41a640e51f30a71825e8
blattodephobia <hselldf398@gmail.com>
2019-04-20 23:56:28 +0300

    First unit test for previously existing logic.

---
+++ Minesweeper.Extensions/ArrayExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+    public static class ArrayExtensions
+    {
+        public static TTarget[,] SelectArray<TSource, TTarget>(this TSource[,] sourceArray, Func<TSource, TTarget> projection)
+        {
+            int columns = sourceArray.GetLength(0);
+            int rows = sourceArray.GetLength(1);
+            var result = new TTarget[columns, rows];
+
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    result[x, y] = projection.Invoke(sourceArray[x, y]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Minesweeper.Tests/Minesweeper.Tests/Components/MineFieldViewModelTests.cs
@@ -0,0 +1,72 @@
+﻿using Minesweeper.Extensions;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Components
+{
+    [TestFixture]
+    public class MineFieldViewModelTests
+    {
+        private static Mock<IMineSquare>[,] GenerateMockSquares(ushort rows, ushort columns, Action<Mock<IMineSquare>> mockSetup = null)
+        {
+            var result = new Mock<IMineSquare>[columns, rows];
+
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var mock = new Mock<IMineSquare>();
+                    mockSetup?.Invoke(mock);
+                    result[x, y] = mock;
+                }
+            }
+
+            return result;
+        }
+
+        private static Mock<IMineSquare>[,] GenerateStraightForwardTestField()
+        {
+            return GenerateMockSquares(2, 2, m =>
+            {
+                m.SetupAllProperties();
+                m.Object.Enabled = true;
+                m.Setup(x => x.RevealBomb()).Verifiable();
+            });
+        }
+
+        [Test]
+        public void DisablesExistingSquaresWhenCellExploded()
+        {
+            Mock<IMineSquare>[,] mocks = GenerateStraightForwardTestField();
+            IMineSquare[,] field = mocks.SelectArray(x => x.Object);
+            var vm = new MineFieldViewModel(new FieldConfiguration(), field);
+
+            field[0, 0].State = MineCellState.Exploded;
+            vm.MineCellStateChanged.Execute(field[0, 0]);
+
+            Assert.IsTrue(field.OfType<IMineSquare>().All(x => !x.Enabled));
+            foreach (var mock in mocks.OfType<Mock<IMineSquare>>())
+            {
+                mock.VerifyAll();
+            }
+        }
+
+        [Test]
+        public void EndsGameWhenCellExploded()
+        {
+            Mock<IMineSquare>[,] mocks = GenerateStraightForwardTestField();
+            IMineSquare[,] field = mocks.SelectArray(x => x.Object);
+            var vm = new MineFieldViewModel(new FieldConfiguration(), field);
+
+            field[0, 0].State = MineCellState.Exploded;
+            vm.MineCellStateChanged.Execute(field[0, 0]);
+
+            Assert.AreEqual(MinesweeperGameState.GameLost, vm.GameState);
+        }
+    }
+}
